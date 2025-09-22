ff7d9c359fc17b52c2423dd347b5055b7b9d4fcc
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2025-09-22 06:21:48 -0300

    feat: add validation for required lead form fields

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -31,3 +31,41 @@ test('não deve cadastrar um lead com e-mail inválido', async ({ page }) => {
 
     await expect(page.locator('.alert')).toHaveText('Email incorreto')
 })
+
+test('não deve cadastrar um lead quando o nome não é preenchido', async ({ page }) => {
+    await page.goto('http://localhost:3000')
+
+    await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+    await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+    await page.getByPlaceholder('Seu email principal').fill('gushenriquet@gmail.com')
+    await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+    await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('não deve cadastrar um lead quando o e-mail não é preenchido', async ({ page }) => {
+    await page.goto('http://localhost:3000')
+
+    await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+    await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+    await page.getByPlaceholder('Seu nome completo').fill('Gustavo Henrique')
+    await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+    await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('não deve cadastrar um lead quando nenhum campo obrigatório é preenchido', async ({ page }) => {
+    await page.goto('http://localhost:3000')
+
+    await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+    await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+    await page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+
+    await expect(page.locator('.alert')).toHaveText(['Campo obrigatório', 'Campo obrigatório'])
+})
